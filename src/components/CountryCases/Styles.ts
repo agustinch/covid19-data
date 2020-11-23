ed5d73e3012452b4b98aae2b60080d2d29f22886
Agustin <chg.agustin@gmail.com>
2020-11-22 22:19:27 -0300

    Fix style issues

--- src/components/CountryCases/Styles.ts
+++ src/components/CountryCases/Styles.ts
@@ -6,17 +6,25 @@ export const CountryCasesContainer = styled.div`
   min-height: 400px;
   color: white;
   text-align: center;
-  padding: 40px;
   -webkit-box-shadow: inset 0px 0px 15px 6px rgba(0, 0, 0, 0.2);
   box-shadow: inset 0px 0px 15px 6px rgba(0, 0, 0, 0.2);
   h1 {
     font-weight: 700;
   }
+  &:before {
+    content: '';
+    transform: skewY(-1deg);
+    transform-origin: top left;
+    display: block;
+    width: 100%;
+    background-color: #1c212e;
+    height: 50px;
+  }
 
   @media (max-width: 432px) {
     .checkbox-budget:checked + label,
     .checkbox-budget:not(:checked) + label {
-      font-size: 40px;
+      font-size: 30px;
     }
   }
 `;
